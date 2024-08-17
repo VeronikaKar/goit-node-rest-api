4fd1434c90f37de61cfadc7fe8304dd12fdb432a
VeronikaKaraul <veronika.life@gmail.com>
2024-08-17 14:08:06 +0300

    updated controllers, services, schema

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,16 +1,34 @@
 import Joi from "joi";
 
-
-const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+export const contactAddSchema = Joi.object({
+  name: Joi.string().required().messages({
+    "any.required": "Name is required",
+  }),
+  email: Joi.string().email().required().messages({
+    "any.required": "Email is required",
+    "string.email": "Email must be a valid email address",
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": "Phone number is required",
+  }),
+  favorite: Joi.boolean(),
 });
 
-
-const updateContactSchema = Joi.object({
+export const contactUpdateSchema = Joi.object({
   name: Joi.string(),
-  email: Joi.string().email(),
+  email: Joi.string().email().messages({
+    "string.email": "Email must be a valid email address",
+  }),
   phone: Joi.string(),
-}).or("name", "email", "phone"); 
-export { contactSchema, updateContactSchema };
+  favorite: Joi.boolean(),
+})
+  .or("name", "email", "phone", "favorite")
+  .messages({
+    "object.missing": "At least one field must be updated",
+  });
+
+export const contactUpdateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required().messages({
+    "any.required": "Favorite status is required",
+  }),
+});
