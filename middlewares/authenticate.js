338025578fa79a0667c45b34be2bb389b4740d38
VeronikaKaraul <veronika.life@gmail.com>
2024-08-27 00:02:23 +0300

    update authenticate Db

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,24 +1,23 @@
 import HttpError from "../helpers/HttpError.js";
-
 import { verifyToken } from "../helpers/jwt.js";
-
 import { findUser } from "../services/authServices.js";
 
-const authenticate = async (req, res, next) => {
+const authenticate = async (req, _res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
     return next(HttpError(401, "Authorization header not found"));
   }
 
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
-    return next(HttpError(401, "Bearer not found"));
+  if (bearer !== "Bearer" || !token) {
+    return next(HttpError(401, "Invalid authorization format"));
   }
 
   const { data, error } = verifyToken(token);
   if (error) {
     return next(HttpError(401, error.message));
   }
+
   const { id } = data;
   const user = await findUser({ _id: id });
   if (!user) {
@@ -26,7 +25,7 @@ const authenticate = async (req, res, next) => {
   }
 
   if (!user.token) {
-    return next(HttpError(401, "User alredy logout"));
+    return next(HttpError(401, "User already logged out"));
   }
 
   req.user = user;
